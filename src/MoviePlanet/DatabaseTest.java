6e04632454e44aa75f5f5107aec0c067bc83749f
Patrik Durasek <pxd7081@rit.edu>
2016-11-07 19:01:10 +0100

    Updating basic files

--- src/MoviePlanet/DatabaseTest.java
+++ src/MoviePlanet/DatabaseTest.java
@@ -32,54 +32,11 @@ public class DatabaseTest
             System.out.println("Unable to connect to the database");
             System.exit(0);
         }
-        try
-        {
-            db.printDBInfo();
-            db.printTableInfo("trip");
-        }
-        catch (DLException e)
-        {
-            System.out.println("Unable to print the database/table info\n");
-        }
-        try
-        {
-            db.printResultSetInfo("SELECT * from trip;");
-        }
-        catch (DLException e)
-        {
-            System.out.println("Unable to analyze the query \n");
-        }
 
-        Equipment equipment = new Equipment(9999);
-
-        equipment.setDb(db);
-        System.out.print("Fetched data: ");
-        equipment.fetch(false);
-        equipment.setEquipID(894);
-        System.out.print("Fetched data: ");
-        equipment.fetch(false);
-        System.out.print("Fetched data: ");
-        // testing the new getData method, if value passed to fetch is true, column headers will be added
-        equipment.fetch(true);
-        System.out.print("Fetched data: ");
-        // testing the new getData method, if value passed to fetch is true, column headers will be added
-        equipment.fetchP(true);
-
-        equipment.setEquipID(1);
-        System.out.print("Insert data: ");
-        equipment.postP();
-        equipment.setEquipmentName("Unicycle");
-        equipment.setEquipmentDescription("Dat Boi");
-        System.out.print("Updated data: ");
-        equipment.put();
-        equipment.setEquipmentName("Motor Bike");
-        System.out.print("Updated data: ");
-        equipment.putP();
-        equipment.deleteP();
         try
         {
             db.close();
-            System.out.println("Connection closed successfully");
+            System.out.println("Connection closed successfully!");
         }
         catch (DLException e)
         {
